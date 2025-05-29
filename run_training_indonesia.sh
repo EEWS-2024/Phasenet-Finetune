32cc272b92e94c3b42975f79d6ae815f39bd6474
SyahrulApr86 <apriansyah.syahrul@gmail.com>
2025-05-29 09:52:58 +0000

    refactor: remove 99pct

--- run_training_indonesia.sh
+++ run_training_indonesia.sh
@@ -82,7 +82,7 @@ if [ $? -eq 0 ]; then
             --test_dir="../$DATASET_DIR/npz_padded" \
             --test_list="../$DATASET_DIR/valid_list_99pct.csv" \
             --model_dir="../$OUTPUT_MODEL_DIR" \
-            --output_dir="../test_results_indonesia_99pct" \
+            --output_dir="../$OUTPUT_MODEL_DIR/test_results_indonesia" \
             --batch_size=2 \
             --plot_results
         
